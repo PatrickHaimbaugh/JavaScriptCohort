d21e16a7b60505a0b5480ab03eeea83f63d1324e
Patrick Haimbaugh <Phaimbaugh2000@gmail.com>
2017-06-07 14:16:07 -0400

    added more fizzbuzz examples

--- javascriptfundamentals/005_conditionals.js
+++ javascriptfundamentals/005_conditionals.js
@@ -44,3 +44,23 @@ while(i <= 100)
 	}
 	i++;
 }
+
+// var num = 0;
+
+// while(num <= 100)
+// {
+// 	if(num % 5 === 0 && num % 3 === 0)
+// 	{
+// 		console.log("FizzBuzz");
+// 	}else if(num % 3 === 0){
+// 		console.log("Fizz");
+// 	}else if(num % 5 === 0){
+// 		console.log("Buzz");
+// 	}else{
+// 		console.log(num);
+// 	}
+
+// 	num = num + 1;
+// }
+
+// for(i=0;i<100;)console.log((++i%3?'':'Fizz')+(i%5?'':'Buzz')||i)
