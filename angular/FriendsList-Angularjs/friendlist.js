ec22a199e747526b5165c4a725fd0b79e66e73f7
Patrick Haimbaugh <Phaimbaugh2000@gmail.com>
2017-06-20 12:02:12 -0400

    finished angular.js friendlist

---
+++ angular/FriendsList-Angularjs/friendlist.js
@@ -0,0 +1,28 @@
+(function(){
+	var app = angular.module('friendlist', ['ngRoute'])
+
+	.constant('API_BASE', 'http://rest.learncode.academy/api/jameshandshoe/friends')
+
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/landing-page.html'
+			})
+			.when('/friendlist', {
+				templateUrl: 'views/friendlist.html',
+				controller: 'FriendListController',
+				controllerAs: 'friendlist',
+				resolve: {
+					friends:['FriendListService', function(FriendListService) {
+							return FriendListService.fetchAll();
+						}
+					]
+				}
+			}).otherwise({
+				redirectTo: '/landing-page'
+			})
+			.when('/landing-page', {
+				templateUrl: 'views/landing-page.html',
+			});
+	}]);
+})();
